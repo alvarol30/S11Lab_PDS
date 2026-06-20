dcd91b658cb684c146a583f4943d8009650cf689
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-20 17:15:54 -0500

    feat: implemente la clase asbtracta Cafe

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,2 +1,12 @@
-public class Cafe {
+public abstract class Cafe{
+    private String descripcion= "Cafe simple";
+
+    public Cafe(String descripcion){
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public abstract double costo();
 }
