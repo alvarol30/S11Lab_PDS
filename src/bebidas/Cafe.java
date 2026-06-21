93517f9f3a2ae776d3d22b5b1738a21c74830226
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-21 16:58:30 -0500

    feat: modifique la clase Cafe para añadir la nueva funcionalidad del sistema con el hash de clave - valor

--- src/bebidas/Cafe.java
+++ src/bebidas/Cafe.java
@@ -1,10 +1,25 @@
 package bebidas;
 
+import java.util.HashMap;
+
 public abstract class Cafe{
-    public String descripcion= "bebidas.Cafe simple";
+    public String descripcion= "Cafe simple";
+
+    public HashMap<String, Double> tamano= new HashMap<String, Double>(){{
+        put("N", 0.0);
+        put("M", 0.0);
+        put("G", 0.0);
+    }};
+
+    public String TamanoDefault= "N";
+
+    public Cafe(String tamano){
+        this.TamanoDefault= tamano;
+    }
 
     public String getDescripcion(){
         return descripcion;
     }
+
     public abstract double costo();
 }
