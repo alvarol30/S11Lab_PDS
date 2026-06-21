2916d5834178c417f5169a426251ad4a4b081a3e
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-21 17:19:34 -0500

    feat: hice lo mismo con la clase Expreso, aplicar el hash e implementar

--- src/bebidas/Descafeinado.java
+++ src/bebidas/Descafeinado.java
@@ -1,18 +1,18 @@
 package bebidas;
 
- import java.util.HashMap;
+import java.util.HashMap;
 
 public class Descafeinado extends Cafe{
     public Descafeinado(String tamano){
         super(tamano);
         descripcion= "Cafe Descafeinado";
-        this.TamanoDefault = tamano;
         this.precios = new HashMap<String, Double>() {{
             put("N", 1.05);
             put("M", 1.15);
             put("G", 1.25);
         }};
     }
+    @Override
     public double costo(){
         return precios.get(TamanoDefault);
     }
--- src/bebidas/Expreso.java
+++ src/bebidas/Expreso.java
@@ -1,10 +1,19 @@
 package bebidas;
 
-public class Expreso extends Cafe {
-    public Expreso(){
-        descripcion= "bebidas.Cafe bebidas.Expreso";
+import java.util.HashMap;
+
+public class Expreso extends Cafe{
+    public Expreso(String tamano){
+        super(tamano);
+        descripcion= "Cafe Expreso";
+        precios= new HashMap<String, Double>(){{
+            put("N", 1.99);
+            put("M", 2.09);
+            put("G", 2.15);
+        }};
     }
+    @Override
     public double costo(){
-        return 1.99;
+        return precios.get(TamanoDefault);
     }
 }
