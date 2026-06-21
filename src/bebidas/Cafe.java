67324bdc53c9f1ba19584690b768be8cc5c71f26
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-21 17:11:05 -0500

    fix: correji algunos errores que salian en Cafe con sus atributos

--- src/bebidas/Cafe.java
+++ src/bebidas/Cafe.java
@@ -4,13 +4,7 @@ import java.util.HashMap;
 
 public abstract class Cafe{
     public String descripcion= "Cafe simple";
-
-    public HashMap<String, Double> precios= new HashMap<String, Double>(){{
-        put("N", 0.0);
-        put("M", 0.0);
-        put("G", 0.0);
-    }};
-
+    public HashMap<String, Double> precios= new HashMap<>();
     public String TamanoDefault= "N";
 
     public Cafe(String tamano){
