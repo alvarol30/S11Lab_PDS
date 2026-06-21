3e1ecee657b3c71fc843367c7427658ce9ced283
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-21 17:43:09 -0500

    feat: implemente ahora la clase DecoratorComplemento de acorde con las demas clases padre

--- src/complementos/DecoratorComplemento.java
+++ src/complementos/DecoratorComplemento.java
@@ -2,7 +2,13 @@ package complementos;
 
 import bebidas.Cafe;
 
-public abstract class DecoratorComplemento extends Cafe {
+public abstract class DecoratorComplemento extends Cafe{
+    protected Cafe cafe;
+
+    public DecoratorComplemento(Cafe cafe){
+        super(cafe.TamanoDefault);
+        this.cafe=cafe;
+    }
     public abstract String getDescripcion();
 
 }
