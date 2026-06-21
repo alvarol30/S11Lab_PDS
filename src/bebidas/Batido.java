07112fdfa88be5d9a7d6c5d053b2fdb89ce2cf9a
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-21 17:24:42 -0500

    feat: y ahora con las clases que quedaban de tipos de cafes, misma implementacion con hash

--- src/bebidas/Batido.java
+++ src/bebidas/Batido.java
@@ -1,10 +1,20 @@
 package bebidas;
 
-public class Batido extends Cafe {
-    public Batido(){
-        descripcion= "bebidas.Cafe bebidas.Batido";
+import java.util.HashMap;
+
+public class Batido extends Cafe{
+    public Batido(String tamano){
+        super(tamano);
+        descripcion= "Cafe Batido";
+        precios= new HashMap<String, Double>(){{
+            put("N", 0.89);
+            put("M", 0.99);
+            put("G", 1.09);
+        }};
     }
+
+    @Override
     public double costo(){
-        return 0.89;
+        return precios.get(TamanoDefault);
     }
 }
--- src/bebidas/TostadoNegro.java
+++ src/bebidas/TostadoNegro.java
@@ -1,11 +1,20 @@
 package bebidas;
 
-public class TostadoNegro extends Cafe {
-    public TostadoNegro(){
-        descripcion= "bebidas.Cafe Tostado Negro";
+import java.util.HashMap;
+
+public class TostadoNegro extends Cafe{
+    public TostadoNegro(String tamano){
+        super(tamano);
+        descripcion= "Cafe Tostado Negro";
+        precios= new HashMap<String, Double>(){{
+            put("N", 0.99);
+            put("M", 1.09);
+            put("G", 1.19);
+        }};
     }
 
+    @Override
     public double costo(){
-        return 0.99;
+        return precios.get(TamanoDefault);
     }
 }
